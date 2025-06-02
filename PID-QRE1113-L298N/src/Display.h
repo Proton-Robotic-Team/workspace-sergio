2ef4048d99a452afd8e90b816fe72de0300a902a
Sérgio Beltrão <sergio.bt.souza@gmail.com>
2025-06-02 15:16:46 -0300

    prototipo de funcao nao mais usada removido

--- PID-QRE1113-L298N/src/Display.h
+++ PID-QRE1113-L298N/src/Display.h
@@ -3,7 +3,6 @@
 
 void display(const char *titulo, const char *valor, bool menuAnterior, bool menuPosterior);
 void display(const char *titulo);
-bool faixaDeMenus(short valorMenu);
 void iniciarDisplay();
 void limparDisplay();
 
